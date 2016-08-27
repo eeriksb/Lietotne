95b1d2298e633bc9745da8a45f12bd0765197ae7
Eriks Berzins <eeriksb@gmail.com>
2016-08-27 13:41:25 +0300

    ...

---
+++ Receptes/Receptes/Pamat_info.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace Receptes
+{
+    public partial class Pamat_info : ContentPage
+    {
+        public Pamat_info()
+        {
+            InitializeComponent();
+        }
+    }
+}
